214dc4e482dc998b4a567bad1ce04db74c1ec8c5
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-25 10:57:02 +0100

    inseriti macro-componenti

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
 
+    <siteHeader/>
+    <!-- 
+       / HEADER
+     -->
+
+    <siteMain/>
+    <!-- 
+       / MAIN
+     -->
+
+    <siteFooter/>
+    <!-- 
+        / FOOT.
+     -->
+
   </div>
 </template>
 
 <script>
+import siteHeader from "./components/siteHeader.vue";
+import siteMain from "./components/siteMain.vue";
+import siteFooter from "./components/siteFooter.vue";
 
 export default {
   name: 'App',
+    components: {
+      siteHeader,
+      siteMain,
+      siteFooter,
+    }
   
 }
 </script>
 
 <style lang="scss">
+@import "../node_modules/bootstrap";
 
 </style>
